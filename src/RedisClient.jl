959bdebd0185d88e08017827cecae89a84664f8e
Mark Moloney <markmo@me.com>
2014-11-02 11:06:20 +1100

    flesh out RedisClient API, tests, and docs

--- src/RedisClient.jl
+++ src/RedisClient.jl
@@ -3,9 +3,14 @@
 
 module RedisClient
 
+using Docile
 using Logging
 
 Logging.configure(level=DEBUG)
+@docstrings
+
+const REDIS_ERR = -1
+const REDIS_OK = 0
 
 const REDIS_REPLY_STRING = 1
 const REDIS_REPLY_ARRAY = 2
@@ -15,6 +20,7 @@ const REDIS_REPLY_STATUS = 5
 const REDIS_REPLY_ERROR = 6
 
 redisContext = 0
+pipelinedCommandCount = 0
 
 type RedisReadTask
     rtype::Int32
@@ -109,139 +115,452 @@ function start_session(host::String = "127.0.0.1", port::Int64 = 6379)
     global redisContext = ccall((:redisConnect, "libhiredis"), Ptr{Uint8}, (Ptr{Uint8}, Int32), host, port)
 end
 
+function end_session()
+    if redisContext != 0
+        ccall((:redisFree, "libhiredis"), Void, (Ptr{Uint8},), redisContext)
+    end
+end
+
+@doc "Free memory allocated to objects returned from hiredis" ->
+function free_reply_object(redisReply)
+    ccall((:freeReplyObject, "libhiredis"), Void, (Ptr{RedisReply},), redisReply)
+end
+
+@doc """
+Appends commands to an output buffer. Pipelining is sending a batch of commands
+to redis to be processed in bulk. It cuts down the number of network requests.
+""" ->
+function pipeline_command(command::String)
+    if redisContext == 0
+        start_session()
+    end
+    debug(string("RedisClient.pipeline_command: ", command))
+    global pipelinedCommandCount += 1
+    ccall((:redisAppendCommand, "libhiredis"), Void, (Ptr{Uint8}, Ptr{Uint8}), redisContext, command)
+end
+
+@doc """
+In a blocking context, this function first checks if there are unconsumed
+replies to return and returns one if so. Otherwise, it flushes the output
+buffer to the socket and reads until it has a reply.
+""" ->
+function call_get_reply(redisReply::Array{Ptr{RedisReply}})
+    ccall((:redisGetReply, "libhiredis"), Int32, (Ptr{Uint8}, Ptr{Ptr{RedisReply}}), redisContext, redisReply)
+end
+
+@doc """
+Calls call_get_reply until the pipelinedCommandCount is 0 or an error
+is returned. Adds the results from each reply to an Array, then returns the
+Array.
+""" ->
+function get_reply()
+    redisReply = Array(Ptr{RedisReply}, 1)
+    results = []
+    while pipelinedCommandCount > 0 && call_get_reply(redisReply) == REDIS_OK
+        push!(results, get_result(redisReply[1]))
+        global pipelinedCommandCount -= 1
+    end
+    results
+end
+
+@doc """
+Converts the reply object from hiredis into a String, int, or Array
+as appropriate the the reply type.
+""" ->
+function get_result(redisReply::Ptr{RedisReply})
+    r = unsafe_load(redisReply)
+    if r.rtype == REDIS_REPLY_ERROR
+        error(bytestring(r.str))
+    end
+    ret = nothing
+    if r.rtype == REDIS_REPLY_STRING
+        ret = bytestring(r.str)
+    elseif r.rtype == REDIS_REPLY_INTEGER
+        ret = int(r.integer)
+    elseif r.rtype == REDIS_REPLY_ARRAY
+        results = []
+        n = int(r.elements)
+        replies = pointer_to_array(r.element, n)
+        for i in 1:n
+            ri = unsafe_load(replies[i])
+            push!(results, bytestring(ri.str))
+        end
+        ret = results
+    end
+    free_reply_object(redisReply)
+    ret
+end
+
+macro pipeline(expr)
+    args = ex.args |> filter(x -> typeof(x) == Expr) |> map(x ->
+        begin
+            fn = x.args[1]
+            if fn in names(RedisClient)
+                args = x.args[2:end]
+                push!(args, "pipeline=true")
+                Expr(fn, args)
+            else
+                x
+            end
+        end)
+    eval(Expr(:block, args))
+end
+
+@doc "Issues a blocking command to hiredis." ->
 function do_command(command::String)
     if redisContext == 0
-        error("redisContext not defined. Please call RedisClient.start_session.")
+#         error("redisContext not defined. Please call RedisClient.start_session.")
+        start_session()
     end
     debug(string("RedisClient.do_command: ", command))
     redisReply = ccall((:redisCommand, "libhiredis"), Ptr{RedisReply}, (Ptr{Uint8}, Ptr{Uint8}), redisContext, command)
-    r = unsafe_load(redisReply)
-    if r.rtype == REDIS_REPLY_ERROR
-        error(bytestring(r.str))
-    end
-    ret = nothing
-    if r.rtype == REDIS_REPLY_STRING
-        ret = bytestring(r.str)
-    elseif r.rtype == REDIS_REPLY_INTEGER
-        ret = int(r.integer)
-    elseif r.rtype == REDIS_REPLY_ARRAY
-        results = []
-        n = int(r.elements)
-        replies = pointer_to_array(r.element, n)
-        for i in 1:n
-            ri = unsafe_load(replies[i])
-            push!(results, bytestring(ri.str))
-        end
-        ret = results
-    end
-    ccall((:freeReplyObject, "libhiredis"), Void, (Ptr{RedisReply},), redisReply)
-    ret
+    get_result(redisReply)
 end
 
+@doc "Issues a blocking command to hiredis, accepting command arguments as an Array." ->
 function do_command(argv::Array)
+    if redisContext == 0
+        start_session()
+    end
     redisReply = ccall((:redisCommandArgv, "libhiredis"), Ptr{RedisReply}, (Ptr{Uint8}, Int32, Ptr{Ptr{Uint8}}, Ptr{Uint}), redisContext, length(argv), argv, C_NULL)
-    r = unsafe_load(redisReply)
-    if r.rtype == REDIS_REPLY_ERROR
-        error(bytestring(r.str))
+    get_result(redisReply)
+end
+
+@doc "Switches between blocking and pipelined command execution according to flag." ->
+function docommand(cmd, pipeline)
+    pipeline || pipelinedCommandCount > 0 ? pipeline_command(cmd) : do_command(cmd)
+end
+
+# @doc "Set the string value of a key." Dict{Any,Any}(
+#     :params => Dict(
+#         :key           => "The key to set",
+#         :value         => "The value to set",
+#         :ex            => "Set the specified expire time, in seconds",
+#         :px            => "Set the specified expire time, in milliseconds",
+#         :pipeline      => "A flag to indicate that the command should be pipelined"
+#     )) ->
+@doc md"""
+Set the string value of a key.
+
+Params:
+
+* key - The key to set
+* value - The value to set
+* ex - Set the specified expire time, in seconds
+* px - Set the specified expire time, in milliseconds
+* pipeline - A flag to indicate that the command should be pipelined
+""" ->
+function kvset(key::String, value::String; ex=0, px=0, pipeline=false)
+    cmd = string("SET ", key, " ", value)
+    if ex > 0
+        cmd = string(cmd, " EX ", ex)
+    elseif px > 0
+        cmd = string(cmd, " PX ", ex)
     end
-    ret = nothing
-    if r.rtype == REDIS_REPLY_STRING
-        ret = bytestring(r.str)
-    elseif r.rtype == REDIS_REPLY_INTEGER
-        ret = int(r.integer)
-    elseif r.rtype == REDIS_REPLY_ARRAY
-        results = []
-        n = int(r.elements)
-        replies = pointer_to_array(r.element, n)
-        for i in 1:n
-            ri = unsafe_load(replies[i])
-            push!(results, bytestring(ri.str))
-        end
-        ret = results
-    end
-    ccall((:freeReplyObject, "libhiredis"), Void, (Ptr{RedisReply},), redisReply)
-    ret
+    docommand(cmd, pipeline)
 end
 
-function kvset(key::String, value::String)
-    do_command(string("SET ", key, " ", value))
+@doc "Get the value of a key." ->
+function kvget(key::String; pipeline=false)
+    docommand(string("GET ", key), pipeline)
 end
 
-function kvget(key::String)
-    do_command(string("GET ", key))
+@doc "Increment the integer value of a key by one." ->
+function incr(key::String; pipeline=false)
+    docommand(string("INCR ", key), pipeline)
 end
 
-function incr(key::String)
-    do_command(string("INCR ", key))
+@doc "Delete a key." ->
+function del(key::String)
+    docommand(string("DEL ", key))
 end
 
-function hset(key::String, attr_name::String, attr_value)
+@doc "Determine if a key exists." ->
+function exists(key::String)
+    docommand(string("DEL ", key))
+end
+
+@doc "Find all keys matching the given pattern." ->
+function keys(pattern::String)
+    docommand(string("KEYS ", pattern))
+end
+
+@doc "Return a serialized version of the value stored at the specified key." ->
+function dump(key::String)
+    docommand(string("DUMP ", key))
+end
+
+@doc "Determine the type stored at key." ->
+function rtype(key::String)
+    docommand(string("TYPE ", key))
+end
+
+@doc "Set the string value of a hash field." ->
+function hset(key::String, attr_name::String, attr_value; pipeline=false)
     #TODO do_command(["HSET %s %s %s", key, attr_name, string(attr_value)])
-    do_command(string("HSET ", key, " ", attr_name, " ", attr_value))
+    docommand(string("HSET ", key, " ", attr_name, " ", attr_value), pipeline)
 end
 
-function hget(key::String, attr_name::String)
-    do_command(string("HGET ", key, " ", attr_name))
+@doc "Get the value of a hash field." ->
+function hget(key::String, attr_name::String; pipeline=false)
+    docommand(string("HGET ", key, " ", attr_name), pipeline)
 end
 
-function hmset(key::String, argv...)
+@doc """
+Set multiple hash fields to multiple values. A variable number of arguments
+follow key in `field` `value` format, e.g.:
+    `hmset("myhash", "field1", "Hello", "field2", "World")`
+""" ->
+function hmset(key::String, argv...; pipeline=false)
     cmd = string("HMSET ", key)
     for arg in argv
         cmd = string(cmd, " ", arg)
     end
-    do_command(cmd)
+    docommand(cmd, pipeline)
 end
 
-function hmset(key::String, attrs::Array)
+@doc """
+Set multiple hash fields to multiple values. Fields and values are provided
+as an Array.
+""" ->
+function hmset(key::String, attrs::Array; pipeline=false)
     cmd = string("HMSET ", key)
     for attr in attrs
         cmd = string(cmd, " ", attr)
     end
-    do_command(cmd)
+    docommand(cmd, pipeline)
 end
 
-function hmget(key::String, argv...)
+@doc """
+Set multiple hash fields to multiple values. Fields and values are provided
+as a Dict.
+""" ->
+function hmset(key::String, attrs::Dict; pipeline=false)
+    cmd = string("HMSET ", key)
+    for (field, val) in attrs
+        cmd = string(cmd, " ", field, " ", val)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc """
+Get the values of all the given hash fields. Multiple fields are provided
+as additional arguments.
+""" ->
+function hmget(key::String, argv...; pipeline=false)
     cmd = string("HMGET ", key)
     for arg in argv
         cmd = string(cmd, " ", arg)
     end
-    do_command(cmd)
+    docommand(cmd, pipeline)
 end
 
-function hmget(key::String, fields::Array)
+@doc """
+Get the values of all the given hash fields. Multiple fields are provided
+as an Array.
+""" ->
+function hmget(key::String, fields::Array; pipeline=false)
     cmd = string("HMGET ", key)
     for field in fields
         cmd = string(cmd, " ", field)
     end
-    do_command(cmd)
+    docommand(cmd, pipeline)
 end
 
+@doc "Get all the fields and values in a hash." ->
 function hgetall(key::String)
-    do_command(string("HGETALL ", key))
+    reply = do_command(string("HGETALL ", key))
+    n = length(reply)
+    dict = Dict()
+    if n > 1 && mod(n, 2) == 0
+        for i = 1:2:n
+            dict[reply[i]] = reply[i + 1]
+        end
+    end
+    dict
 end
 
-function sadd(key::String, member)
-    do_command(string("SADD ", key, " ", string(member)))
+@doc """
+Delete one or more hash fields. Multiple fields are provided
+as additional arguments.
+""" ->
+function hdel(key::String, argv...; pipeline=false)
+    cmd = string("HDEL ", key)
+    for arg in argv
+        cmd = string(cmd, " ", arg)
+    end
+    docommand(cmd, pipeline)
 end
 
-function smembers(key::String)
-    do_command(string("SMEMBERS ", key))
+@doc """
+Delete one or more hash fields. Multiple fields are provided
+as an Array.
+""" ->
+function hdel(key::String, fields::Array; pipeline=false)
+    cmd = string("HDEL ", key)
+    for field in fields
+        cmd = string(cmd, " ", field)
+    end
+    docommand(cmd, pipeline)
 end
 
-export start_session, kvset, kvget, incr, hset, hget, hmset, hmget, hgetall, sadd, smembers
-
+@doc "Determine if a hash field exists." ->
+function hexists(key::String, field::String; pipeline=false)
+    docommand(string("HEXISTS ", key, " ", field), pipeline)
 end
 
-# start_session("127.0.0.1", 6379)
-# incr("last_customer_key")
-# set("foo", "bar")
-# get("foo")
-# hset("customer:1", "first_name", "Mark")
-# hget("customer:1", "first_name")
-# hmset("customer:1", "first_name", "Alice", "last_name", "Bean")
-# hmget("customer:1", "first_name", "last_name")
-# hgetall("customer:1")
-# sadd("account_keyset:1", 1)
-# sadd("account_keyset:1", 2)
-# sadd("account_keyset:1", 3)
-# smembers("account_keyset:1")
+@doc "Get all the fields in a hash." ->
+function hkeys(key::String; pipeline=false)
+    docommand(string("HKEYS ", key), pipeline)
+end
+
+@doc "Get all the values in a hash." ->
+function hvals(key::String; pipeline=false)
+    docommand(string("HVALS ", key), pipeline)
+end
+
+@doc "Get the number of fields in a hash." ->
+function hlen(key::String; pipeline=false)
+    docommand(string("HLEN ", key), pipeline)
+end
+
+@doc "Increment the integer value of a hash field by the given number." ->
+function hincrby(key::String, field::String, increment::Int64; pipeline=false)
+    docommand(string("HINCRBY ", key, " ", field, " ", increment), pipeline)
+end
+
+@doc "Increment the float value of a hash field by the given number." ->
+function hincrby(key::String, field::String, increment::Float64; pipeline=false)
+    docommand(string("HINCRBYFLOAT ", key, " ", field, " ", increment), pipeline)
+end
+
+@doc "Add a member to a set." ->
+function sadd(key::String, member; pipeline=false)
+    docommand(string("SADD ", key, " ", string(member)), pipeline)
+end
+
+@doc "Get all the members in a set." ->
+function smembers(key::String; pipeline=false)
+    docommand(string("SMEMBERS ", key), pipeline)
+end
+
+@doc "Determine if a given value is a member of a set." ->
+function sismember(key::String, member; pipeline=false)
+    docommand(string("SISMEMBER ", key, " ", member), pipeline)
+end
+
+@doc "Get the number of members in a set." ->
+function scard(key::String; pipeline=false)
+    docommand(string("SCARD ", key), pipeline)
+end
+
+@doc """
+Remove one or more members from a set. Members are provided
+as additional arguments.
+""" ->
+function srem(key::String, argv...; pipeline=false)
+    cmd = string("SREM ", key)
+    for arg in argv
+        cmd = string(cmd, " ", arg)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc """
+Remove one or more members from a set. Members are provided
+as an Array.
+""" ->
+function srem(key::String, members::Array; pipeline=false)
+    cmd = string("SREM ", key)
+    for member in members
+        cmd = string(cmd, " ", member)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Subtract multiple sets. Multiple sets are provided as additional arguments." ->
+function sdiff(key::String, argv...; pipeline=false)
+    cmd = string("SDIFF ", key)
+    for arg in argv
+        cmd = string(cmd, " ", arg)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Subtract multiple sets. Multiple sets are provided as an Array." ->
+function sdiff(keys::Array; pipeline=false)
+    cmd = "SDIFF"
+    for key in keys
+        cmd = string(cmd, " ", key)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Intersect multiple sets. Multiple sets are provided as additional arguments." ->
+function sinter(key::String, argv...; pipeline=false)
+    cmd = string("SINTER ", key)
+    for arg in argv
+        cmd = string(cmd, " ", arg)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Intersect multiple sets. Multiple sets are provided as an Array." ->
+function sinter(keys::Array; pipeline=false)
+    cmd = "SINTER"
+    for key in keys
+        cmd = string(cmd, " ", key)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Add multiple sets. Multiple sets are provided as additional arguments." ->
+function sunion(key::String, argv...; pipeline=false)
+    cmd = string("SUNION ", key)
+    for arg in argv
+        cmd = string(cmd, " ", arg)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Add multiple sets. Multiple sets are provided as an Array." ->
+function sunion(keys::Array; pipeline=false)
+    cmd = "SUNION"
+    for key in keys
+        cmd = string(cmd, " ", key)
+    end
+    docommand(cmd, pipeline)
+end
+
+@doc "Change the selected database for the current connection." ->
+function selectdb(db)
+    do_command(string("SELECT ", db))
+end
+
+@doc "Remove all keys from the current database." ->
+function flushdb()
+    do_command("FLUSHDB")
+end
+
+@doc "Remove all keys from all databases." ->
+function flushall()
+    do_command("FLUSHALL")
+end
+
+export
+    # session
+    start_session, end_session,
+    # key-value
+    kvset, kvget, incr, del, exists, keys, dump, rtype,
+    # hash sets
+    hset, hget, hmset, hmget, hgetall, hdel, hexists, hkeys, hvals, hlen, hincrby,
+    # sets
+    sadd, smembers, sismember, scard, srem, sdiff, sinter, sunion
+    # management
+    selectdb, flushdb, flushall,
+    # pipelining
+    get_reply,
+    # generic
+    do_command, pipeline_command
+
+end
