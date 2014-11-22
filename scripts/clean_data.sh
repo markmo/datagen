521916524bd7c60301f051335df9fa07f5005b2c
Mark Moloney <markmo@me.com>
2014-11-23 10:24:11 +1100

    add script to clean data

--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rsync -azv $SCRIPTDIR/../data/t_*.csv datagen@10.64.116.73:/var/staging/
+rm -f $SCRIPTDIR/../data/*.csv
